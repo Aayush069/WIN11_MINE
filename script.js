b415838ace17b53c90b50f4a326114c83c5f8d1d
Aayush069 <86056158+Aayush069@users.noreply.github.com>
2021-06-30 20:12:36 +0530

    1st

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ windowsicon.addEventListener('click',()=>{
     }
     else{
         startmenu.style.bottom  = '50px'
+        searchmenu.style.bottom='-655px'
+        
+    }
+})
+
+
+
+let searchicon=document.getElementsByClassName('searchicon')[0]
+let searchmenu=document.getElementsByClassName('searchmenu')[0]
+
+searchicon.addEventListener('click',()=>{
+    if (searchmenu.style.bottom == '50px'){
+        searchmenu.style.bottom = '-655px'
+        
+    }
+    else{
+        searchmenu.style.bottom  = '50px'
+        startmenu.style.bottom='-655px'
     }
 })
